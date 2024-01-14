7c3f7e89cdadbc40a20608f479d0b3e8b5928275
Dilsat Varli <dilsatvarli@hotmail.co.uk>
2024-01-14 21:49:35 +0000

    Add meny and show menu functions

--- run.py
+++ run.py
@@ -17,3 +17,43 @@ contactdetails = SHEET.worksheet('contactdetails')
 data = contactdetails.get_all_values()
 
 print(data)
+
+
+def show_menu():
+    """
+    Function to display menu items to user.
+    This function is called from the menu function.
+    """
+    print("\nWelcome to your Contact Book!\n")
+    print("Main menu")
+    print("-----------------")
+    print("1) See all Contacts")
+    print("2) Add new Contact")
+    print("3) Search Contact")
+    print("4) Edit Contact\n")
+
+
+def menu():
+    """
+    User selects which task they would like to do, uses their input and runs
+    elif loop to trigger the next process.
+    If an invalid choice is input then the programme
+    will alert user and ask for another choice.
+    """
+    while True:
+        show_menu()
+        choice = input('Please enter a choice from the above numbers: ')
+        if choice == '1':
+            retrieve_all_contact()
+        elif choice == '2':
+            add_new_contact()
+        elif choice == '3':
+            search_contact()
+        elif choice == '4':
+            edit_contact()
+        else:
+            print(f'Not a valid choice: <{choice}>,try again')
+
+
+if __name__ == '__main__':
+    menu()
